0f4ccb89290804c6c82ad99c7a629f8321b9f5c5
Marcelo Melo <marcelomelo.ciandt@sulamerica.com.br>
2021-07-24 16:58:09 -0300

    Add DateUtil

--- src/main/java/practice/ciandt/springboot/util/DateUtil.java
+++ src/main/java/practice/ciandt/springboot/util/DateUtil.java
@@ -1,2 +1,13 @@
-package practice.ciandt.springboot.util;public class DateUtil {
+package practice.ciandt.springboot.util;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class DateUtil {
+    public String formatLocalDateTimeToDatabaseStyle(LocalDateTime localDateTime) {
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(localDateTime);
+    }
 }
