b95251f449ae206d741760ba0fe706e6982e9b8e
Marcelo Melo <marcelomelo.ciandt@sulamerica.com.br>
2021-07-24 16:42:36 -0300

    First commit

---
+++ src/main/java/practice/ciandt/springboot/PracticeSpringbootApplication.java
@@ -0,0 +1,13 @@
+package practice.ciandt.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practice/ciandt/springboot/PracticeSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.ciandt.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
